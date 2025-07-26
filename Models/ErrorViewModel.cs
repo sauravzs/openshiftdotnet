5618a812681c0f024bd372f07d9188f998ea743b
Saurav Kumar <k.saurav.in@gmail.com>
2025-07-27 01:03:03 +0530

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace openshiftdotnet.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
